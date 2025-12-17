6f958ee61d757665213fbe8e72c6541321feb56b
Adamma11 <adamma.nithya@gmail.com>
2025-12-17 15:14:00 +0530

    Add files via upload

---
+++ BACKEND_LISTENER/models/data_entry/request_for_payment_education.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const RequestforPaymentEducationSchema = mongoose.Schema({
+    case:{type:mongoose.Schema.Types.ObjectId,ref:'Case'},
+mov:{type:String},movRhs:{type:String},
+comments:[],
+colorType:{type:String},
+    componentName:{type:String},
+    componentType:{type:String},
+    componentDisplayName:{type:String},
+    componentId:{type:String},
+    inFavourOf:{type:String},
+    modeOfPayment:{type:String},
+   amount:{type:String},
+
+    requestDate:{type:Date},
+    status:{type:String},
+    paymentReferenceNumber:String,
+    requestCompletionDate:{type:Date},
+    modifiedBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    modifiedOn:{type:Date,default:Date.now()}
+})
+module.exports = mongoose.model('RequestforPaymentEducation',RequestforPaymentEducationSchema);
