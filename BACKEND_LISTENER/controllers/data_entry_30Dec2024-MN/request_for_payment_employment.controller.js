7a1fd4d42f9a9283e19a2f9f3b60b29ad13da8e7
Adamma11 <adamma.nithya@gmail.com>
2025-12-17 12:33:03 +0530

    Add files via upload

---
+++ BACKEND_LISTENER/controllers/data_entry_30Dec2024-MN/request_for_payment_employment.controller.js
@@ -0,0 +1,102 @@
+const RequestForPaymentEmployment = require('../../models/data_entry/request_for_payment_employment.model')
+const express = require('express');
+
+
+exports.create=(req,res)=>{
+
+    if(!req.body.case){
+        res.json({message:"Case Id is Mandatory"})
+    }
+    if(!req.body.componentName){
+        res.json({message:"Component Name Cannot be Empty"})
+    }
+    if(!req.body.componentId){
+        res.json({message:"Component Id Cannot be Empty"})
+    }    
+    if(!req.body.inFavourOf){
+        res.json({message:"In favour of Cannot be Empty"})
+    }        
+    if(!req.body.modeOfPayment){
+        res.json({message:"Mode of Payment Cannot be Empty"})
+    }            
+    if(!req.body.amount){
+        res.json({message:"Amount Cannot be Empty"})
+    }            
+    const requestForPaymentEmployment = new RequestForPaymentEmployment({
+        case:req.body.case,
+        componentName:req.body.componentName,
+        componentId:req.body.componentId,
+        inFavourOf:req.body.inFavourOf,
+        modeOfPayment:req.body.modeOfPayment,
+        amount:req.body.amount,
+        requestDate:Date.now(),
+        status:'INITIATED',
+        modifiedBy:req.user.user_id,
+    })
+    requestForPaymentEmployment
+    .save(requestForPaymentEmployment)
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message:err.message | "Some error occurred while saving Request for Payment for Education Record"
+        })
+    })
+}
+exports.readAllForAComponent=(req,res)=>{
+    RequestForPaymentEmployment
+    .find({case:req.params.case,componentName:req.params.componentName,componentId:req.params.componentId})
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message:err.message | "An error occurred while reading Request for Payment for Employment details"
+        })
+    })
+};
+exports.readAllWithStatus=(req,res)=>{
+    RequestForPaymentEmployment
+    .find({status:req.params.status})
+    .populate({path:'case',populate:{path:'subclient',populate:{path:'client'}}})
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message:err.message | "An error occurred while reading Request for Payment for Employment details"
+        })
+    })
+}
+
+exports.readASingleComponent=(req,res)=>{
+    RequestForPaymentEmploment
+    .findOne({_id:req.params._id})
+    .populate({path:'case',populate:{path:'subclient',populate:{path:'client'}}})
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message:err.message | "An error occurred while reading Request for Payment for Employment details"
+        })
+    })
+};
+
+exports.updatePaymentReferenceNumber=(req,res)=>{
+    RequestForPaymentEmployment
+    .findOneAndUpdate({_id:req.params._id},{
+        paymentReferenceNumber:req.body.paymentReferenceNumber,
+        requestCompletionDate:new Date(),
+        status:"COMPLETED"
+    })
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message:err.message | "An error occcurred while updating the Status"
+        })
+    })
+};
