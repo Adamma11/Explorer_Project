8b06fa9db8554ef8bd137bd96638e73815e5921c
Adamma11 <adamma.nithya@gmail.com>
2025-12-17 17:58:53 +0530

    Add files via upload

---
+++ REPORTS-LISTENER/models/data_entry/personal_details_data.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const PersonalDetailsDataSchema = mongoose.Schema({
+case:{type:mongoose.Schema.Types.ObjectId,ref:'Case',required:true},
+mov:{type:String},movRhs:{type:String},
+name:{type:String},
+fathername:{type:String},
+dateofbirth:{type:Date},
+number:{type:String},
+emailid:{type:String},
+empid:{type:String},
+uniqueid:{type:String},
+employeeid:{type:String},
+doj:{type:Date},
+process:{type:String},
+location:{type:String},
+aadhernumber:{type:String},
+pancard:{type:String},
+status:{type:String},
+dataEntryComments:{type:String},
+dataEntryCompletionDate:{type:Date},
+inputqcComments:{type:String},
+inputqcCompletionDate:{type:Date},
+modifiedBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+modifiedOn:{type:Date,default:Date.now}
+})
+module.exports = mongoose.model('PersonalDetailsData',PersonalDetailsDataSchema);
