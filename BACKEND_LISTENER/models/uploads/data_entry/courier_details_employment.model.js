bc911a39674d84d3449741242a6ee96fe5dbaacb
Adamma11 <adamma.nithya@gmail.com>
2025-12-17 16:23:54 +0530

    Add files via upload

---
+++ BACKEND_LISTENER/models/uploads/data_entry/courier_details_employment.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const CourierDetailsEmploymentSchema = mongoose.Schema({
+    case:{type:mongoose.Schema.Types.ObjectId,ref:'Case'},
+    componentName:{type:String},
+    componentId:{type:String},
+    courierName:{type:String},
+    awbNumber:{type:String},
+    courierDate:{type:Date},
+    status:{type:String},
+    modifiedBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    modifiedOn:{type:Date,default:Date.now()}
+})
+module.exports = mongoose.model('CourierDetailsEmployment',CourierDetailsEmploymentSchema);
