e77ab8d02c70623b58e021beed10d4f9881e8456
Adamma11 <adamma.nithya@gmail.com>
2025-12-17 11:59:20 +0530

    Add files via upload

---
+++ BACKEND_LISTENER/config/db.conf.js
@@ -0,0 +1,16 @@
+//  module.exports = {
+//      url:"mongodb://3.6.175.176/cvws-new?keepAlive=true&maxPoolSize=500&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000"
+//  };
+//module.exports = {
+  // url:"mongodb://127.0.0.1/cvws-new?keepAlive=true&maxPoolSize=500&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000"
+//};
+//14 10 2025
+/*module.exports = {
+   url:"mongodb://43.205.162.85/cvws-new?keepAlive=true&maxPoolSize=500&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000"
+};*/
+// 14 10 2025
+
+module.exports = {
+  url: "mongodb://43.205.162.85:27017/cvws-new"
+};
+
