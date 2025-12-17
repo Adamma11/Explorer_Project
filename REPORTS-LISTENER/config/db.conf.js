a95e71b10d673bb3f89eaa9c5ba8a898686f9ec3
Adamma11 <adamma.nithya@gmail.com>
2025-12-17 17:33:38 +0530

    Add files via upload

---
+++ REPORTS-LISTENER/config/db.conf.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     url:"mongodb://3.6.175.176/cvws-new?keepAlive=true&maxPoolSize=100&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000"
+// };
+module.exports = {
+    url:"mongodb://127.0.0.1/cvws-new"
+ }
+
+// module.exports = {
+//    url:"mongodb://43.205.162.85/cvws-new?keepAlive=true&maxPoolSize=500&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000"
+// };
+ 
