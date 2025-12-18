e8b38de1b16a5591669e54f943a4607123036164
Adamma11 <adamma.nithya@gmail.com>
2025-12-18 10:45:41 +0530

    Add files via upload

---
+++ REPORTS-LISTENER/models-20Sep2024/data_entry-29Oct2022/request_for_payment_employment.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const RequestforPaymentEmploymentSchema = mongoose.Schema({
+    case:{type:mongoose.Schema.Types.ObjectId,ref:'Case'},
+    componentName:{type:String},
+    componentType:{type:String},
+    componentDisplayName:{type:String},
+    componentId:{type:String},
+    inFavourOf:{type:String},
+    modeOfPayment:{type:String},
+    amount:{type:String},
+    requestDate:{type:Date},
+    status:{type:String},
+    paymentReferenceNumber:String,
+    requestCompletionDate:{type:Date},
+    modifiedBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    modifiedOn:{type:Date,default:Date.now()}
+})
+module.exports = mongoose.model('RequestforPaymentEmployment',RequestforPaymentEmploymentSchema);
