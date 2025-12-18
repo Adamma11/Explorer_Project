42f91caee90fade750a71a22eeb6963c03b3c471
Adamma11 <adamma.nithya@gmail.com>
2025-12-18 10:56:30 +0530

    Add files via upload

---
+++ REPORTS-LISTENER/models-20Sep2024/sales/lead_or_prospect_contact.model_nr.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const LeadOrProspectContactSchema = mongoose.Schema({
+    name:{type:String,required:true},
+    leadOrProspect:{type:mongoose.Schema.Types.ObjectId,ref:'LeadOrProspect'},
+    designation:{type:String},
+    phone:{type:String},
+    emailId:{type:String},
+    modifiedBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    modifiedOn:{type:Date,default:Date.now()}
+}) 
+module.exports = mongoose.model("LeadOrProspectContact",LeadOrProspectContactSchema);
