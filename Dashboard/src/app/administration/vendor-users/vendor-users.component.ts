f72da69cfdee2283b0c29236d007ec92e6649e55
Adamma11 <adamma.nithya@gmail.com>
2025-12-18 15:39:10 +0530

    Add files via upload

---
+++ Dashboard/src/app/administration/vendor-users/vendor-users.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-users',
+  templateUrl: './vendor-users.component.html',
+  styleUrls: ['./vendor-users.component.scss']
+})
+export class VendorUsersComponent {
+
+}
