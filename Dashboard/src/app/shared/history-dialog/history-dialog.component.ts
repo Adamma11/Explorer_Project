f4ecde2b865ad8efefc212b4dab4498a0e15565f
Adamma11 <adamma.nithya@gmail.com>
2025-12-23 17:14:30 +0530

    Add files via upload

---
+++ Dashboard/src/app/shared/history-dialog/history-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-dialog',
+  templateUrl: './history-dialog.component.html',
+  styleUrls: ['./history-dialog.component.scss']
+})
+export class HistoryDialogComponent {
+
+}
